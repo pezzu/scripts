6dc7ef196afa478c696c977532443f598ce16cf7
pezzu <peter.sukhenko@gmail.com>
2018-08-16 22:47:47 +0300

    Code moved to user dir

--- code.cmd
+++ code.cmd
@@ -3,14 +3,14 @@
 if not "%1"=="" goto params
 
 if not exist .vscode if not exist .git if not exist .hg if not exist .svn (
-    start "code" "C:\Program Files\Microsoft VS Code\Code.exe"
+    start "code" "C:\Users\peter\AppData\Local\Programs\Microsoft VS Code\Code.exe"
     goto :eof
 )
 
-start "code" "C:\Program Files\Microsoft VS Code\Code.exe" .
+start "code" "C:\Users\peter\AppData\Local\Programs\Microsoft VS Code\Code.exe" .
 goto :eof
 
 :params
-start "code" "C:\Program Files\Microsoft VS Code\Code.exe" %*
+start "code" "C:\Users\peter\AppData\Local\Programs\Microsoft VS Code\Code.exe" %*
 
 
