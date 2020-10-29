15a853de9fd0bf335704cf1b4fa7e86bbb29cd72
pezzu <7243796+pezzu@users.noreply.github.com>
2020-10-29 18:57:13 -0400

    Correction for mongodb run in docker

--- mongod.cmd
+++ mongod.cmd
@@ -1,6 +1,6 @@
 @rem start "mongo" "C:\Program Files\MongoDB\Server\3.4\bin\mongod.exe" --dbpath d:\data\db
 
 @rem docker volume create --name=mongodb-storage
-@rem docker container create --name mongodb -p 27017:27017 -v mongodb-storage:/data/db mongo
+@rem docker container create --name mongodb -p 27017:27017 --mount 'type=volume,src=mongodb-storage,dst=/data/db' mongo
 
-docker run -d -p 27017:27017 -v d:/data/db:/data/db mongo
+docker container start mongodb
